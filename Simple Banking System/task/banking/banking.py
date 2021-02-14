6857a1f5a9d31920ac10e9219aece6f5bd2d8e30
zrus <->
2021-02-14 18:15:43 +0700

    stage 3

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3 as db
 
 # Write your code here
 MAIN_MENU_CHOOSE = 1
@@ -6,10 +7,15 @@ ACCOUNT_MENU_CHOOSE = 2
 LOGIN_INPUT = 3
 EXITING = 4  # Technical final state for exit
 
+DB_NAME = "card.s3db"
+TABLE_NAME = "card"
+
 
 class BankMachine:
     def __init__(self):
-        self.accounts = {}
+        self.db_conn = None
+
+        # self.accounts = {}
         self.state = MAIN_MENU_CHOOSE
         self.current_account = None
 
@@ -54,23 +60,36 @@ class BankMachine:
         else:
             return '0'
 
+    def try_insert_new_card(self, card_num, pin):
+        row = self.db_conn.execute(f"SELECT * FROM {TABLE_NAME} where number = ? ;", (card_num,)).fetchone()
+        if row:
+            return
+        else:
+            try:
+                cur = self.db_conn.execute(f"INSERT INTO {TABLE_NAME} (number, pin) VALUES (?,?)",
+                                           (card_num, pin))
+                self.db_conn.commit()
+                return cur.lastrowid
+            except db.Error as e:
+                print(e)
+
     def create_account(self):
         random.seed()
-        while True:
+        tries_count = 0
+
+        while tries_count < 3:
             new_card_num = "400000"
             new_card_num += str(random.randint(0, 999999999)).rjust(9, '0')
             new_card_num += str(self.check_sum_card_num(new_card_num))
+            pin_code = str(random.randint(0, 9999)).rjust(4, '0')
 
-            if new_card_num not in self.accounts:
-                new_account = {'card_num': new_card_num,
-                               'pin_code': str(random.randint(0, 9999)).rjust(4, '0'),
-                               'balance': 0
-                               }
-                self.accounts[new_card_num] = new_account
+            new_id = self.try_insert_new_card(new_card_num, pin_code)
+            if new_id:
                 print("\nYour card has been created")
                 print(f"Your card number:\n{new_card_num}")
-                print(f"Your card PIN:\n{new_account['pin_code']}")
+                print(f"Your card PIN:\n{pin_code}")
                 return
+            tries_count += 1
 
     def clear_login_info_buffer(self):
         self.login_info_buffer = {'card_num': "", 'pin_code': ""}
@@ -79,11 +98,24 @@ class BankMachine:
         self.current_account = None
         self.clear_login_info_buffer()
 
+#    def load_account(self, log_info):
+#        if log_info['card_num'] in self.accounts:
+#            account = self.accounts[log_info['card_num']]
+#            if account['pin_code'] == log_info['pin_code']:
+#                return account, "OK"
+#        return None, "Wrong card number or PIN!"
+
     def load_account(self, log_info):
-        if log_info['card_num'] in self.accounts:
-            account = self.accounts[log_info['card_num']]
-            if account['pin_code'] == log_info['pin_code']:
-                return account, "OK"
+        cur = self.db_conn.execute(
+                f"SELECT id, number, balance FROM {TABLE_NAME} where number = ? and pin = ? ;",
+                    (log_info['card_num'],
+                     log_info['pin_code']))
+        row = cur.fetchone()
+        if row:
+            account = {'card_num': row[1],
+                       'id': row[0],
+                       'balance': row[2]}
+            return account, "OK"
         return None, "Wrong card number or PIN!"
 
     def process_account_action(self, line):
@@ -145,14 +177,46 @@ class BankMachine:
         elif self.state == LOGIN_INPUT:
             self.process_login_input(line)
 
+    def check_connection(self):
+        if self.db_conn:
+            self.db_conn.close()
+
+        try:
+            self.db_conn = db.connect(DB_NAME)
+        except db.Error as e:
+            print(e)
+            self.state = EXITING
+            return
+
+        table_exists = self.db_conn.execute(
+            f"SELECT name FROM sqlite_master WHERE type='table' AND name='{TABLE_NAME}';").fetchone()
+
+        if not table_exists:
+            try:
+                self.db_conn.execute(f"create table {TABLE_NAME} ("
+                                     "      id      INTEGER,"
+                                     "      number  TEXT,"
+                                     "      pin     TEXT,"
+                                     "      balance INTEGER DEFAULT 0"
+                                     ");")
+                self.db_conn.commit()
+
+            except db.Error as e:
+                print(e)
+                self.state = EXITING
+
     def machine_run(self):
+        self.check_connection()
+
         while self.state != EXITING:
             self.show_screen()
             line = input()
             self.process_user_input(line)
 
+        if self.db_conn:
+            self.db_conn.close()
+
 
 bm = BankMachine()
 #  print(bm.check_sum_card_num("400000735080216"))
 bm.machine_run()
-
